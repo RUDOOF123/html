72984217e021d513891f5a18249703c2d81fcb0c
Alexis2284 <164031361+RUDOOF123@users.noreply.github.com>
2024-09-02 08:36:21 -0300

    .kjb

--- cosas java/douu.js
+++ cosas java/douu.js
@@ -1,10 +1,4 @@
-/*function trestristestigres() {
-   document.write("tres tristes tigres")
-    for(i=0; i<=3; i++) {
-        document.write("hola mundo");
-    }
-    
-}*/
+
 function suma(num1,num2){
   
 return num1+num2
@@ -25,17 +19,16 @@ function division (num1,num2){
 return num1/num2
 }
 
-/*let num = parseInt(prompt("ingrese el numero a multiplicar"));
-for(i=0; i<=10; i++){
- multi=num*i;
- document.write("    ",num," ","X"," ",i,"=",multi,"<br>");
-}*/
 let num1= parseInt(prompt("ingrese el primer numero"));
 let num2= parseInt(prompt("ingrese el segundo numero"));
+
+
 const ressuma=suma(num1,num2);
 const resresta=resta(num1,num2);
 const resmulti=multiplicacion(num1,num2); 
 const resdivision=division(num1,num2);
+
+
 document.getElementById('ressuma').innerHTML="suma: "+ressuma;
 document.getElementById('resresta').innerHTML="resta: "+resresta;
 document.getElementById('resmulti').innerHTML="multiplicacion: "+resmulti;
